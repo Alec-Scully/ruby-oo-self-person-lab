a85c06eda118dcaf5f1f2cf23af8c13dc4a9c8c1
Oumaima Azzat <oumaimazzat@gmail.com>
2020-11-18 17:11:58 -0500

    peer1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name 
+    attr_accessor :bank_account, :happiness 
+
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name=name 
+        @bank_account=bank_account
+        @happiness=happiness
+
+    end 
+
+    def happiness
+        if @happiness > 10 
+            @happiness = 10 
+         elsif @happiness < 0 
+               @happiness = 0 
+         else 
+            @happiness=@happiness
+         end
+    end 
+
+end 
